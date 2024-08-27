04ad8b3dcfbca35804c2917af8cfa74c36812a1a
LARISAMOSE <mosevica@gmail.com>
2024-08-27 23:58:11 +0200

    add table

--- divanpars/divanpars/spiders/HomeWork.py
+++ divanpars/divanpars/spiders/HomeWork.py
@@ -1,18 +1,37 @@
-import scrapy
+import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+# Настройка браузера Selenium
+driver = webdriver.Chrome()
+url = "https://divan.ru/category/svet"
+driver.get(url)
 
-class Divannewpars1Spider(scrapy.Spider):
-    name = "divannewpars1"
-    allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://divan.ru/category/svet"]
+time.sleep(5)  # Ждем загрузки страницы
 
-    def parse(self, response):
-        svets = response.css("div._Ud0k")
-        for svet in svets:
-            yield{
-                "name" : svet.css("div.lsooF span::text").get(),
-                "price" : svet.css("div.pY3d2 span::text").get(),
-                "url" : svet.css("a").attrib["href"]
-            }
+# Поиск элементов с товарами
+svets = driver.find_elements(By.CLASS_NAME, "_Ud0k")
 
+# Создаем или открываем CSV-файл для записи данных
+with open("output.csv", "w", newline='', encoding="utf-8") as csvfile:
+    fieldnames = ["Название", "Цена", "Link"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
 
+    # Парсинг информации о каждом товаре
+    for svet in svets:
+        name = svet.find_element(By.CLASS_NAME, "lsooF").find_element(By.TAG_NAME, "span").text
+        price = svet.find_element(By.CLASS_NAME, "pY3d2").find_element(By.TAG_NAME, "span").text
+        url = svet.find_element(By.TAG_NAME, "a").get_attribute("href")
+
+        # Запись данных в CSV
+
+        writer.writerow({
+            "Название": name,
+            "Цена": price,
+            "Link": url
+        })
+
+# Закрытие браузера после выполнения задачи
+driver.quit()
