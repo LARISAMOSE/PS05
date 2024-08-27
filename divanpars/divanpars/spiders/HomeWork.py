7ca3de830b3fcbc6d460d69f676ef4152a14284b
LARISAMOSE <mosevica@gmail.com>
2024-08-27 15:26:30 +0200

    add PS05

---
+++ divanpars/divanpars/spiders/HomeWork.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class Divannewpars1Spider(scrapy.Spider):
+    name = "divannewpars1"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://divan.ru/category/svet"]
+
+    def parse(self, response):
+        svets = response.css("div._Ud0k")
+        for svet in svets:
+            yield{
+                "name" : svet.css("div.lsooF span::text").get(),
+                "price" : svet.css("div.pY3d2 span::text").get(),
+                "url" : svet.css("a").attrib["href"]
+            }
+
+
